949f403ad4e81a03e82afdee3b5eab75e4bb4614
Chen-Hao WANG <a0903571712@gmail.com>
2025-11-06 16:53:05 +0800

    1

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,7 @@ set_project("MyVulkanProject")
 set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
 
+add_requires("vulkansdk")
 
 add_requires("glfw")
 add_requires("glm")
@@ -10,11 +11,19 @@ add_requires("stb")
 add_requires("tinygltf")
 add_requires("ktx")
 
-add_packages("vulkan")
+-- Make Vulkan-Hpp structs aggregates so C++20 designated initializers work with MSVC
+add_defines("VULKAN_HPP_NO_STRUCT_CONSTRUCTORS")
+
+-- Note: we rely on the Vulkan SDK via add_requires("vulkansdk") and link it per-target below.
+
+-- Enable Vulkan validation layers automatically in Debug builds
+if is_mode("debug") then
+    add_defines("ENABLE_VALIDATION_LAYERS")
+end
 
 target("VulkanTutorial")
     set_kind("binary")
     
     add_files("src/*.cpp")
 
-    add_packages("glfw", "glm", "tinyobjloader", "stb", "tinygltf", "ktx")
+    add_packages("glfw", "glm", "tinyobjloader", "stb", "tinygltf", "ktx", "vulkansdk")

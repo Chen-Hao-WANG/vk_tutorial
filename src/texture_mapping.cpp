c858dff0c6aa873a20c19016c7e77829668bc8fb
Howard <a0903571712@gmail.com>
2025-11-25 00:59:19 +0800

    nothing gonna change but my love for you

---
+++ src/texture_mapping.cpp
@@ -0,0 +1,8 @@
+#include "tutorial.hpp"
+/*
+create texture image we need:
+1. create a image object which backed by device memory
+2. fill the object with pixel data
+3. create a sampler to sample the image in the shader
+4. add a combined image sampler descriptor to sample from the texture
+*/

3658c987c641a45eaa056c652e7eeb969e124b62
Howard <a0903571712@gmail.com>
2025-12-09 10:11:16 +0800

    fix  vk::ImageUsageFlagBits in position image creation

--- src/gBuffer.cpp
+++ src/gBuffer.cpp
@@ -2,19 +2,19 @@
 
 void HelloTriangleApplication::createGbufferResources()
 {
-    
-    //
+
+    // position
     createImage(
         swapChainExtent.width,
         swapChainExtent.height,
         vk::Format::eR32G32B32A32Sfloat,
         vk::ImageTiling::eOptimal,
-        vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eInputAttachment,
+        vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eStorage,
         vk::MemoryPropertyFlagBits::eDeviceLocal,
         gBufferPositionImage,
         gBufferPositionImageMemory);
     gBufferPositionImageView = createImageView(gBufferPositionImage, vk::Format::eR32G32B32A32Sfloat, vk::ImageAspectFlagBits::eColor);
-
+    // normal
     createImage(swapChainExtent.width, swapChainExtent.height, vk::Format::eR32G32B32A32Sfloat,
                 vk::ImageTiling::eOptimal,
                 vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eStorage,
